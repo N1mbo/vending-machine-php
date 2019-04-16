b59a99d249a8247869afdfb53be8e413c4295c0b
Kacper Pawelec <kp332436@students.mimuw.edu.pl>
2019-04-16 20:28:42 +0200

    Initial

--- src/VendingMachine/VendingMachine.php
+++ src/VendingMachine/VendingMachine.php
@@ -7,8 +7,65 @@ namespace VendingMachine;
 final class VendingMachine {
     function __construct() {}
 
+    const $coin_values = [
+      "NICKEL" -> 0.05,
+      "DIME" -> 0.1,
+      "QUARTER" -> 0.25,
+      "DOLLAR" -> 1.0
+    ]
+
+    const $item_values = [
+      "A" -> 0.65,
+      "B" -> 1.0,
+      "C" -> 1.5
+    ];
+
+    public $available_items = [
+      "A" -> 0,
+      "B" -> 0,
+      "C" -> 0
+    ];
+    public $currently_inserted_money = 0.0; #pocket
+    public $available_change = [
+      "NICKEL" -> 0,
+      "DIME" -> 0,
+      "QUARTER" -> 0,
+      "DOLLAR" -> 0
+    ]
+
+    public function insert_coin($coin_type)
+    {
+      if (in_array($coin_type, $coin_values)) {
+        $available_change[$coin_type] += 1;
+        $currently_inserted_money += $coin_values[$coin_type];
+      }
+    }
+
+    public function get_inserted_money()
+    {
+      return $currently_inserted_money;
+    }
+
+    public function get_coin_count($coin_type)
+    {
+      if (in_array($coin_type, $coin_values))
+        return $available_change[$coin_type];
+      else return 0;
+    }
+
+    public function get_available_items($item_type)
+    {
+      if(in_array($item_type, $available_items))
+        return $available_items[item_type];
+      else return 0;
+    }
+
     public function service(): string
     {
-        return 'no service';
+        $available_items["A"] = 1;
+        $available_items["B"] = 1;
+        $available_items["C"] = 1;
+
+        return;
     }
 }
--- tests/VendingMachine/VendingMachineTest.php
+++ tests/VendingMachine/VendingMachineTest.php
@@ -21,6 +21,23 @@ final class VendingMachineTest extends TestCase
 
     public function testServiceReturnsNoService(): void
     {
-        $this->assertEquals('service', $this->vendingMachine->service());
+        $this->vendingMachine->service();
+
+        $this->assertGreaterThanOrEqual(1, $this->vendingMachine->get_available_items("A"));
+        $this->assertGreaterThanOrEqual(1, $this->vendingMachine->get_available_items("B"));
+        $this->assertGreaterThanOrEqual(1, $this->vendingMachine->get_available_items("C"));
+    }
+
+    public function testInsertCoin(): void
+    {
+      foreach($this->vendingMachine->$coin_values as $coin_type => $value) {
+        $old_amount = $this->vendingMachine->get_coin_count($coin_type);
+        $old_inserted = $this->vendingMachine->get_inserted_money();
+
+        $this->vendingMachine->insert_coin($coin_type);
+
+        $this->assertEquals($old_amount + 1, $this->vendingMachine->get_coin_count($coin_type));
+        $this->assertEquals($old_inserted + $this->vendingMachine->$coin_values[$coin_type], $this->vendingMachine->get_inserted_money());
+      }
     }
 }
